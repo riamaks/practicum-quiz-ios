b16e2466042edd48c875b7eb2453f1aebcb4e765
riamaks <ria.maks.trw@gmail.com>
2024-01-25 20:14:24 +0300

    Выполнение задания по спринту 6

--- MovieQuiz/Models/MostPopularMovies.swift
+++ MovieQuiz/Models/MostPopularMovies.swift
@@ -1,8 +1,29 @@
-//
-//  MostPopularMovies.swift
-//  MovieQuiz
-//
-//  Created by Мария Малышева on 13.12.2023.
-//
-
 import Foundation
+
+struct MostPopularMovies: Codable {
+    let errorMessage: String
+    let items: [MostPopularMovie]
+}
+
+struct MostPopularMovie: Codable {
+    let title: String
+    let rating: String
+    let imageURL: URL
+    
+    var resizedImageURL: URL {
+        let urlString = imageURL.absoluteString
+        let imageUrlString = urlString.components(separatedBy: "._")[0] + "._V0_UX600_.jpg"
+        
+        guard let newURL = URL(string: imageUrlString) else {
+                    return imageURL
+                }
+                
+                return newURL
+            }
+    
+    private enum CodingKeys: String, CodingKey {
+        case title = "fullTitle"
+        case rating = "imDbRating"
+        case imageURL = "image"
+    }
+}
--- MovieQuiz/Services/QuestionFactoryProtocol.swift
+++ MovieQuiz/Services/QuestionFactoryProtocol.swift
@@ -3,4 +3,5 @@ import UIKit
 protocol QuestionFactoryProtocol {
     func requestNextQuestion()
     var delegate: QuestionFactoryDelegate? {get set}
+    func loadData()
 }
